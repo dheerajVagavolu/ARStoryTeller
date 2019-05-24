751bfc164923706d3c18502669396cbecb34e493
V Dheeraj <41289602+dheerajrox@users.noreply.github.com>
2019-05-24 09:32:58 +0530

    Add files via upload
    External Objects can be imported.

--- Part-1_LanguageX_to_BlenderAPI/trans.py
+++ Part-1_LanguageX_to_BlenderAPI/trans.py
@@ -1,8 +1,13 @@
 f = open("code.x")
 wr = open("output.py", "w")
+
+stra = r'D:\blender\models'
+
 wr.write("import bpy\nfrom random import randint\n\n")
 
 skin = ""
+
+external = 0
 
 i = 0
 for x in f:
@@ -19,45 +24,64 @@ for x in f:
 	if determine == "<entity>":
 		print("Entity detected")
 		wr.write("#Object Definition\n")
-	if determine == "$skin":
+	elif determine == "$skin":
 		print("skin detected")
 		a = x.split(':')[1].split(';')[0][1:-1]
-		wr.write("bpy.ops.mesh.primitive_" + a + "_add(location=(0,0,0))\n")
+		print(a)
+		external = 0
 		if a == "cube":
 			skin = "Cube"
-		if a == "cone":
+			wr.write("bpy.ops.mesh.primitive_" + a + "_add(location=(0,0,0))\n")
+		elif a == "cone":
 			skin = "Cone"
-		if a == "uv_sphere":
+			wr.write("bpy.ops.mesh.primitive_" + a + "_add(location=(0,0,0))\n")
+		elif a == "uv_sphere":
 			skin = "Sphere"
-	if determine == "$scale":
+			wr.write("bpy.ops.mesh.primitive_" + a + "_add(location=(0,0,0))\n")
+		else:
+			loc = stra+'\\'+a
+			external = 1
+			wr.write("loc = r'"+loc+"'\nimported_object = bpy.ops.import_scene.obj(filepath=loc)\nobj_object = bpy.context.selected_objects[0]\n")
+
+	elif determine == "$scale":
 		print("scale detected")
 		a = x.split(':')[1].split(';')[0].split(' ')[0].split(',')
 		print(a)
 		wr.write("bpy.context.object.scale[0] = "+a[0]+"\n")
 		wr.write("bpy.context.object.scale[1] = "+a[1]+"\n")
 		wr.write("bpy.context.object.scale[2] = "+a[2]+"\n")
-	if determine == "$init":
+	elif determine == "$init":
 		print("Intitial Position detected")
 		a = x.split(':')[1].split(';')[0].split(' ')[0].split(',')
 		print(a)
 		wr.write("bpy.ops.transform.translate(value=("+a[0]+","+a[1]+","+a[2]+"))\n")
-	if determine == "$rotate":
+	elif determine == "$rotate":
 		print("Intitial Rotation detected")
 		a = x.split(':')[1].split(';')[0].split(' ')[0].split(',')
 		print(a)
 		wr.write("bpy.ops.transform.trackball(value=("+a[0]+","+a[1]+"))\n")
-	if determine == "</entity>":
+	elif determine == "</entity>":
 		print("Entity Ended")
 		wr.write("#Object Definition Complete\n\n")
-	if determine == "@move":
-		print("Move Function detected")
-		a = x.split(':')[1].split(',')
-		print(a)
-		ini_key = a[0]
-		fin_key = a[1]
-		b = x.split(':')[2].split(";")[0].split(',')
-		print(b)
-		wr.write("bpy.data.objects['"+skin+"'].keyframe_insert(data_path='location', frame="+str(ini_key)+")\nbpy.ops.transform.translate(value=("+b[0]+","+b[1]+","+b[2]+"))\nbpy.data.objects['"+skin+"'].keyframe_insert(data_path='location', frame="+str(fin_key)+")")
+	elif determine == "@move":
+		if external == 1:
+			print("Move Function detected")
+			a = x.split(':')[1].split(',')
+			print(a)
+			ini_key = a[0]
+			fin_key = a[1]
+			b = x.split(':')[2].split(";")[0].split(',')
+			print(b)
+			wr.write("bpy.data.objects[obj_object.name].keyframe_insert(data_path='location', frame="+str(ini_key)+")\nbpy.ops.transform.translate(value=("+b[0]+","+b[1]+","+b[2]+"))\nbpy.data.objects[obj_object.name].keyframe_insert(data_path='location', frame="+str(fin_key)+")")
+		else:
+			print("Move Function detected")
+			a = x.split(':')[1].split(',')
+			print(a)
+			ini_key = a[0]
+			fin_key = a[1]
+			b = x.split(':')[2].split(";")[0].split(',')
+			print(b)
+			wr.write("bpy.data.objects['"+skin+"'].keyframe_insert(data_path='location', frame="+str(ini_key)+")\nbpy.ops.transform.translate(value=("+b[0]+","+b[1]+","+b[2]+"))\nbpy.data.objects['"+skin+"'].keyframe_insert(data_path='location', frame="+str(fin_key)+")")
 
 
 
